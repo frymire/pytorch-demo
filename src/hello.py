a750bb4f5d40aa858000c8d6ed26df07fc531df1
Mark Frymire <mark.frymire@icloud.com>
2026-09-03 14:35:00 -0400

    Create a neural network model

--- src/hello.py
+++ src/hello.py
@@ -1,12 +1,31 @@
 
 import torch
 
-from torch import nn
-from torch import device
+from torch import nn, Tensor
+from torch.nn import Flatten, Sequential, Linear, ReLU
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.datasets import FashionMNIST
 from torchvision.transforms import v2
+
+
+class NeuralNetwork(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.flatten: Flatten = Flatten()
+        self.linear_relu_stack: Sequential = Sequential(
+            Linear(28*28, 512),
+            ReLU(),
+            Linear(512, 512),
+            ReLU(),
+            Linear(512, 10)
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x: Tensor = self.flatten(x)
+        logits: Tensor = self.linear_relu_stack(x)
+        return logits
 
 
 def main() -> None:
@@ -38,7 +57,10 @@ def main() -> None:
     print(torch.cuda.is_available())
     accelerator: torch.device | None = torch.accelerator.current_accelerator()
     detected_device: str = accelerator.type if accelerator is not None else "cpu"
-    print(f"Device: {detected_device}")
+    print(f"Using {detected_device} device")
+
+    model: NeuralNetwork = NeuralNetwork().to(detected_device)
+    print(model)
 
 
 if __name__ == "__main__":
